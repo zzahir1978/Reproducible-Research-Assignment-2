15c5963452053680b28ea5262b7e7c05abc2d887
Zahiruddin Zahidanishah <65801670+zzahir1978@users.noreply.github.com>
2020-06-06 21:16:29 +0800

    Upload programming files
    Upload files for report submission:-
    1) RR_Course_Assignment_Project_2.txt
    2) RR_Course_Assignment_Project_2.R
    3) RR_Course_Assignment_Project_2.Rmd

---
+++ RR_Course_Assignment_Project_2.R
@@ -0,0 +1,19 @@
+---
+title: "Environmental Disaster Impact in US"
+author: "Zahiruddin Zahidanishah"
+date: "6/6/2020"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Introduction
+
+Storms and other severe weather events can cause both public health and economic problems for communities and municipalities. Many severe events can result in fatalities, injuries, and property damage, and preventing such outcomes to the extent possible is a key concern.
+
+This project involves exploring the U.S. National Oceanic and Atmospheric Administration's (NOAA) storm database. This database tracks characteristics of major storms and weather events in the United States, including when and where they occur, as well as estimates of any fatalities, injuries, and property damage.
+
+The events in the database start in the year 1950 and end in November 2011. In the earlier years of the database there are generally fewer events recorded, most likely due to a lack of good records. More recent years should be considered more complete.
+
